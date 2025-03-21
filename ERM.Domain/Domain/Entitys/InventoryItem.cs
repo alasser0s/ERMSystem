477c1bced5a5920498dd790f4a0e90da9b41a507
sono <abdelrhmanstorm123@gmail.com>
2025-03-21 21:51:14 +0200

    Schedule -InventoryItem entites

---
+++ ERM.Domain/Domain/Entitys/InventoryItem.cs
@@ -0,0 +1,14 @@
+﻿public class InventoryItem { 
+    public int Id { get; set; }
+    public string Name { get; set; }
+    public string ItemNumber { get; set; }
+    public string Description { get; set; }
+    public int Quantity { get; set; }
+    public decimal UnitPrice { get; set; }
+    public string Location { get; set; }
+    public int MinimumStock { get; set; }
+    public int RecordQuantity { get; set; }
+    public bool IsActive { get; set; }
+    public ICollection<InventoryItemTransaction> InventoryItemTransactions { get; set; }
+
+}
--- ERM.Domain/Domain/Entitys/Schedule.cs
+++ ERM.Domain/Domain/Entitys/Schedule.cs
@@ -1,4 +1,10 @@
 ﻿public class Schedule
 {
-
-}
+    public int Id { get; set; }
+    public Employee employee { get; set; }
+    public DateTime StartTime { get; set; }
+    public DateTime EndTime { get; set; }
+    public string Description { get; set; }
+    public string notes { get; set; }
+    public bool IsAccessed { get; set; }
+    }
