34c06ba33f483a7357821263e3a37a6216f69766
sono <abdelrhmanstorm123@gmail.com>
2025-04-06 02:20:18 +0200

    1 - could filter the assets based on : which asset is active or not which asset is undermaintaince or not which asset is asigned to an employee

---
+++ ERM.Domain/Domain/Interfaces/IAssetRepository.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ERM.Domain.Domain.Interfaces
+{
+    public interface IAssetRepository : IRepository<Asset>
+    {
+Task<IEnumerable<Asset>> GetByStatusAsync(string status);
+
+            Task<IEnumerable<Asset>> GetDueForMaintainanceAsync(DateTime startDate , DateTime endDate);
+        Task<IEnumerable<Asset>> GetAsignedToEmployeeAsync(int employeeId);
+        Task BulkUpdateStatusAsync(IEnumerable<int> assetIds, string status);
+    }
+}
