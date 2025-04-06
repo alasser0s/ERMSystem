37784977c4e750f9cafb099c348a62df1428ed22
sono <abdelrhmanstorm123@gmail.com>
2025-04-06 02:22:07 +0200

    generic abstraction , it will be hyprid avoiding too much debulicated code by minimizing CRUD operators

---
+++ ERM.Domain/Domain/Interfaces/IRepository.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ERM.Domain.Domain.Interfaces
+{
+    public interface IRepository<TEntity> where TEntity : class
+    {
+       Task<TEntity> GetByIdAsync(int Id);
+        Task<IEnumerable<TEntity>> GetAllAsync();
+        Task<TEntity> AddAsync(TEntity entity);
+        Task<TEntity> UpdateAsync(
+            TEntity entity);
+        Task DeleteAsync(int Id);
+    }
+}
