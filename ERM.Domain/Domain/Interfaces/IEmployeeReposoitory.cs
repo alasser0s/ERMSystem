4c41f4ae756d2e9c8df61df3fb0f77e10e417fcb
sono <abdelrhmanstorm123@gmail.com>
2025-04-06 02:23:28 +0200

    you can get an employee and search for one by its email - position - department - and if he's active or not

---
+++ ERM.Domain/Domain/Interfaces/IEmployeeReposoitory.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ERM.Domain.Domain.Interfaces
+{
+    public interface IEmployeeReposoitory : IRepository<Employee>
+    {
+       Task<Employee> GetByEmailAsync(string email);
+       Task<IEnumerable<Employee>> GetByDepartmentIdAsync(int departmentId);
+       Task<IEnumerable<Employee>> GetAvailableEmployeeAsync(DateTime StartDate , DateTime EndDate);
+       Task<IEnumerable<Employee>> GetEmployeeByPositionAsync(int positionId);
+        Task BulkUpdateStatusAsync(IEnumerable<int> employeeids, bool IsActive);
+
+
+    }
+}
